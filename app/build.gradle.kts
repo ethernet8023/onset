9502b74197e7a10a068d9d4fdb55e0c7c1b25f56
Folf <folf@folf.me>
2026-02-14 14:54:05 +0100

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "pw.zotan.psylog"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1"
+        versionCode = 4
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
